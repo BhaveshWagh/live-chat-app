26bfcbace872d5d566170b01a0035bb84b444b6a
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-02-14 12:50:03 +0530

    new origin removed and added old

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin:
-      "https://65cc61eea001e97719eb4483--glistening-wisp-7c60fb.netlify.app/",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
@@ -19,14 +18,15 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected : ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+  socket.on("join_room",(data) => {
+    socket.join(data)
+    console.log(`User with ID: ${socket.id} joined room: ${data}`)
+  })
+
+  socket.on("send_message",(data)=>{
+    socket.to(data.room).emit("receive_message",data)
+  })
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
